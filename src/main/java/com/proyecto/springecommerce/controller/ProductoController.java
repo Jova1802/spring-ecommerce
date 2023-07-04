462cb5d68e0ba30574908791124104f1299a1261
Omar Velez Arteaga <omar.6995@hotmail.com>
2023-07-04 16:45:16 -0500

    Se implementa metodo para Actializar productos y se crea clase para adjuntar una imagen al servidor

--- src/main/java/com/proyecto/springecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/springecommerce/controller/ProductoController.java
@@ -10,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 // se crea controlador de productos
 @Controller
@@ -47,6 +50,23 @@ public class ProductoController {
         return "redirect:/product";
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
 
+        LOGGER.info("Producto encontrado: {}", producto);
 
+        model.addAttribute("producto", producto);
+
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+
+        productoService.update(producto);
+        return "redirect:/product";
+    }
 }
