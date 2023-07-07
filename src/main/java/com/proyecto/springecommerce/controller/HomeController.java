6884ffd8042f571da0bb9aedd1784eb888ac0b9a
Omar Velez Arteaga <omar.6995@hotmail.com>
2023-07-07 17:19:24 -0500

    se crea home para el usuario con funcionalidad para mostrar productos (metodo home, directorio y template home.html)

--- src/main/java/com/proyecto/springecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/springecommerce/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.proyecto.springecommerce.controller;
 
-import com.proyecto.springecommerce.model.Producto;
 import com.proyecto.springecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,20 +7,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("/admin")
-public class AdministradorController {
+@RequestMapping("/")
+public class HomeController {
 
     @Autowired
     private ProductoService productoService;
 
     @GetMapping("")
-    public String home(Model model){
+    public String home(Model model) {
 
         model.addAttribute("productos", productoService.findAll());
-
-        return "administrador/home";
+        return "/usuario/home";
     }
 }
